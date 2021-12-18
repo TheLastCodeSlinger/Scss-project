062ff2095b9a30ef0b0c3a51d6fb2231adb9bbad
Michael Brandt <m.brandt@iib-institut.de>
2021-12-18 20:34:56 +0100

    form styling

--- src/App.js
+++ src/App.js
@@ -15,6 +15,43 @@ const App = () => {
         <SectionFeature />
         <SectionTours />
         <Stories />
+        <section className="section-book">
+          <div className="row">
+            <div className="book">
+              <div className="book__form">
+                <form action="#" className="form">
+                  <div className="u-margin-bottom-medium">
+                    <h2 className="heading-secondary">Start booking now!</h2>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="text"
+                      className="form__input"
+                      placeholder="Full Name"
+                      required
+                      id="name"
+                    />
+                    <label for="name" className="form__label">
+                      Full Name
+                    </label>
+                  </div>
+                  <div className="form__group">
+                    <input
+                      type="email"
+                      className="form__input"
+                      placeholder="Email address"
+                      required
+                      id="email"
+                    />
+                    <label for="email" className="form__label">
+                      Email
+                    </label>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
