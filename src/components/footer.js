f63df83571db7da5ec96dfa396c1ef3862b30c50
Michael Brandt <m.brandt@iib-institut.de>
2021-12-19 16:54:07 +0100

    footer in jsx

---
+++ src/components/footer.js
@@ -0,0 +1,56 @@
+import Logo from '.././img/logo-green-1x.png';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="footer__logo-box">
+      <img src={Logo} alt="Full logo" className="footer__logo" />
+    </div>
+    <div className="row">
+      <div className="col-1-of-2">
+        <div className="footer__navigation">
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Company
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Contact us
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Careers
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Privacy policy
+              </a>
+            </li>
+            <li className="footer__item">
+              <a href="#" className="footer__link">
+                Terms
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="col-1-of-2">
+        <p className="footer__copyright">
+          Built by
+          <a href="#" className="footer__link">
+            Michael Brandt
+          </a>
+          because me great
+          <a href="#" className="footer__link">
+            advanced scss &copy;
+          </a>
+        </p>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
